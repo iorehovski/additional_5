b7cdc4d20834fc16a22aac9e9fa7922fc097fcb6
iorehovski <iorehovski@gmail.com>
2017-09-20 21:07:17 +0300

    20 passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var aBrakets = [];
+//получаем пары скобок
+  for(var i = 0; i < bracketsConfig.length; i++){
+  	var patt = "";
+  	for(var j = 0; j < bracketsConfig[i].length; j++){
+  		patt += bracketsConfig[i][j];
+  	}
+  	aBrakets.push(patt);
+  }
+//удаляем 
+  for(var i = 0; i < str.length; i++){
+  	for(var j = 0; j < aBrakets.length; j++){
+  	  if(str.indexOf(aBrakets[j]) >= 0){
+  	  	str = str.replace(String(aBrakets[j]),'');
+  	  	i--;
+  	  	break;
+  	  }
+  	}
+  }
+  
+  if(str.length === 0)
+  	return true;
+  else 
+  	return false;
 }
